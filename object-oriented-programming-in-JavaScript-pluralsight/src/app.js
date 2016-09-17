2be9727d6c00d008a6e7542055fd4240842649ec
chintu123 <chintu.m18@gmail.com>
2016-09-17 13:47:41 +0530

    Inheritance and Code Organization

--- object-oriented-programming-in-JavaScript-pluralsight/src/app.js
+++ object-oriented-programming-in-JavaScript-pluralsight/src/app.js
@@ -54,21 +54,21 @@
 // console.log("Get Property: " + drone1.Id + "...." + drone1.Name);
 /* Module 2 - Start */
 
-class Vehicle {
-    constructor() {
-        this.isGPSEnabled = false; // Property inheritance. Child class can override base class property values;
-        console.log("baseclass vehicle contructor");
-    }
+// class Vehicle {
+//     constructor() {
+//         this.isGPSEnabled = false; // Property inheritance. Child class can override base class property values;
+//         console.log("baseclass vehicle contructor");
+//     }
 
-    start() { // Method inheritance
-        console.log("starting vehicle");
-    }
+//     start() { // Method inheritance
+//         console.log("starting vehicle");
+//     }
 
-    static staticMethod() {
-        // This method is also inherited and works in same manner as instance methods
-        console.log("static method in baseclass vehicle");
-    }
-}
+//     static staticMethod() {
+//         // This method is also inherited and works in same manner as instance methods
+//         console.log("static method in baseclass vehicle");
+//     }
+// }
 
 // class Drone extends Vehicle {
 //     constructor() {
